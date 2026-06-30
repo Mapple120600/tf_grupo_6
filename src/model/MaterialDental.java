ebaa92e5e74970e072cf46e1f7b850a832bc3a01
Fabrizio24xd <josefabrizio248@gmail.com>
2026-06-30 16:36:40 -0500

    modelo para materiales dentales y su stock

---
+++ src/model/MaterialDental.java
@@ -0,0 +1,27 @@
+package model;
+
+public class MaterialDental {
+    private int id;
+    private String nombre;
+    private int stock;
+    private String unidad;
+
+    public MaterialDental(int id, String nombre, int stock, String unidad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.stock = stock;
+        this.unidad = unidad;
+    }
+
+    public int getId() { return id; }
+    public String getNombre() { return nombre; }
+    public int getStock() { return stock; }
+    public String getUnidad() { return unidad; }
+    public void aumentarStock(int cantidad) { stock += cantidad; }
+    public void disminuirStock(int cantidad) { if (cantidad <= stock) stock -= cantidad; }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + " | Material: " + nombre + " | Stock: " + stock + " " + unidad;
+    }
+}
